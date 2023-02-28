db0936c203fdf040e3f7e009fe47b0009bc5f49c
t-lira <119820325+t-lira@users.noreply.github.com>
2023-02-28 13:37:32 -0300

    Contador de cliques

---
+++ 010-/script.js
@@ -0,0 +1,12 @@
+let contador = 0 
+let res = document.querySelector('section#result')
+
+function contar() {
+    contador ++ 
+    res.innerHTML = `<p>O contador está com <mark>${contador}</mark> cliques.</p>`
+}
+
+function zerar() {
+    contador = 0
+    res.innerHTML = `<p>O contador está com <mark>0 (zero)</mark> cliques.</p>`
+}
